b2c31db4aedc8e714091078bba4db788407d67d9
Craig Celeste <craigc@innosphere.ca>
2014-01-22 23:13:29 -0500

    playing with generation of mocks

--- app/js/controllers/GenerateMockBackendCtrl.js
+++ app/js/controllers/GenerateMockBackendCtrl.js
@@ -1,9 +1,41 @@
 
+// When the responses from web requests are several KB of JSON, it's impractical to create them for each test.
+// I am trying to kill several birds with one stone here.
+
+// 		1. writing code that traverses the web services to make sure they meet my expectations.
+//			This code is adapted into the real request code later.
+
+//		2. capturing the output of the web service into a form that I can adapt for mock data shared by unit tests.
+//			I'll need to do some (hopefully superficial) sanitization of the data so that I don't check in real data to github
+
+//		3. Over the last 3 or 4 years of using Rally, they periodically change their web services in an unexpected (to us) way.
+//			This allows us to a) regenerate the mock data from their new service and do a diff to the old one to very quickly identify the changes.
+//			b) debug through this generation traversal in order to update the code to their new format
+//			c) our testing staff can use this through the website to help debug (they are software testers so are good at it). Understanding the problem may let them to change their data to avoid the bug to continue working
+
+// It's also a bit of a JavaScript exercise for fun.
+
 app.controller('GenerateMockBackendCtrl', ['$log', '$scope', '$q', '$http',  function($log, $scope, $q, $http) {
 	"use strict";
 	$log.debug('Creating GenerateMockBackendCtrl')
 
+	// We'll write the generated code to the scope, which appears in the page, which may be saved.
+	// This temperary message is there until we're done
+
 	$scope.code = "Generating... Watch the console if you're bored..."
+
+	// Collect some data here that will be built into the generated code at the end
+
+	var guids = {};
+	var nextGuidNum = 1;
+	var requests = [];
+	var subscriptionRef = 'https://rally1.rallydev.com/slm/webservice/v3.0/subscription';
+	var subscriptionUrl;
+	var workspacesRef;
+	var workspacesUrl;
+	var projectsRefs = {}; // keyed on workspaceRef
+
+	// Helper functions
 
 	function assert(condition, message) {
 		if (!condition) throw new Error(message);
@@ -14,79 +46,210 @@ app.controller('GenerateMockBackendCtrl', ['$log', '$scope', '$q', '$http',  fun
 		return url + (url.indexOf('?') >= 0 ? '&' : '?') + querystring;
 	}
 
-	function sanitize(data) {
-		// TODO
-		return data;
+	function pad(n, width, z) {
+		z = z || '0';
+		n = n + '';
+		return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 	}
 
-	// Collect some data here that will be built into the generated code at the end
+	function sanitize(data) {
 
-	var requests = [];
-	var subscriptionRef = 'https://rally1.rallydev.com/slm/webservice/v3.0/subscription';
-	var workspacesRef;
-	var workspacesUrl;
-	var projectRef;
-	var projectUrl;
+		var result = JSON.parse(JSON.stringify(data)); // poor mans deep copy. I realize there are better ways.
+		if (!result) return result;
+
+		function process(value, key) {
+			if (typeof value === 'boolean') {
+				return value;
+			}
+
+			if (typeof value === 'number') {
+				return value;
+			}
+
+			if (typeof value !== 'string') {
+				$log.warn('sanitize: unrecognized type/value:', typeof value, value);
+				return value;
+			}
+
+			// replace any guids in the string; keep a dictionary so guids in all future strings get the same new value
+			_.each(value.match(/[0-9a-f]{8}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{12}/g), function(originalGuid) {
+				var keyGuid = originalGuid.replace('-', '');
+				var replacementGuid = guids[keyGuid];
+				if (!replacementGuid) {
+					replacementGuid = guids[keyGuid] = '00000000-0000-0000-0000-' + pad(nextGuidNum++, 12, '0');
+				}
+				value = value.replace(originalGuid, originalGuid.length == 36 ? replacementGuid : replacementGuid.replace('-', ''))
+			})
+
+			// TODO identify any "user text" fields (fields that have user entered text values like Name and Description)
+			//		Dates, Ref's, UUID's, should be fine
+			// 
+			// rename them to Name="Name1", etc
+			// Be smart about keeping some things matched up like _refObjectName and Name
+			// Maybe have a switch by property name (now many can their be?) and a default branch identifies properties I didn't review (and probably nulls them out)
+
+			return value;
+		}
+
+		function traverse(obj) {
+			if (obj) {
+				for (var key in obj) {
+
+					// objects will traverse
+					if (typeof obj[key] === 'object') {
+						if (obj) { // null is an object; don't traverse
+							traverse(obj[key]);
+						}
+					}
+					else {
+						var newKey = process(key);
+						var newValue = process(obj[key], key);
+						delete obj[key];
+						obj[newKey] = newValue;
+					}
+
+				}
+			}
+		}
+
+		if (typeof result === 'object' && result){
+			traverse(result)
+		}
+		else {
+			result = process(result);
+		}
+
+		return result;
+	}
 
 	// Subscription
 
-	$log.info('Querying subscriptionData');
-	var subscriptionUrl = getUrl(subscriptionRef)
+	$log.info('Querying Subscription');
+	subscriptionUrl = getUrl(subscriptionRef)
 	$http.jsonp(subscriptionUrl)
-	.then(function(subscriptionData) {
-		$log.info('subscriptionData', subscriptionData)
+	.then(function(subscriptionReponse) {
+		$log.info('subscriptionReponse', subscriptionReponse)
 
 		requests.push({
 			url: subscriptionUrl,
-			data: sanitize(subscriptionData)
+			data: subscriptionReponse.data
 		})
 
 	// Workspaces
 
-		workspacesRef = subscriptionData.data.Subscription.Workspaces._ref;
+		workspacesRef = subscriptionReponse.data.Subscription.Workspaces._ref;
 		assert(workspacesRef, 'workspacesRef expected');
 
 		$log.info('Querying workspaces list');
 		workspacesUrl = getUrl(workspacesRef, {pagesize:200});
 		return $http.jsonp(workspacesUrl);
-	}).then(function(workspacesData){
-		$log.info('workspacesData', workspacesData)
+	}).then(function(workspacesResponse){
+		$log.info('workspacesResponse', workspacesResponse)
 
 		requests.push({
 			url: workspacesUrl,
-			data: workspacesData 
+			data: workspacesResponse.data
 		})
 
-	// Projects
 
-		// TODO figure out a way to direct the generation down a particular path, without adding details about our data to github.
 
+// TODO recursion idea
+// track 1 workspaceRef, 1 projectRef, 1 iterationRef, etc
+// begin drilling in
+// if it doesn't reach the bottom, fall out of those promises and begin again
+// only end when we have reached the bottom and have 1 of each request type to be scripted.
+// use the recursive function technique used below (queryNextProjectList)
+// nest the recursive functions inside each other.
+
+	function drillIntoWorkspace() {
+		function drillIntoProject() {
+			function drillIntoIteration() {
+				function drillIntoTestSets() {
+					function drillIntoTestSetDetails() {
+					}
+				}
+			}
+		}
+	}
+
+
+
+
+
+
+
+	// Projects - this is a recursive promise; it will load one project and then the next when that is done, and then the next, until done.
+	// 		note that it will drill all the way into Project->Iteration->TestSet, etc before going to the second Project
+	//		I could do them concurrently but I kind of want to stop once I have accumulated enough data and I don't know which projects go deep enough until I check.
+
+		var queuedProjectRequests = _.reduce(workspacesResponse.data.QueryResult.Results, function(memo, workspace) {
+			memo.push({
+				workspaceRef: workspace._ref,
+				projectsRef: workspace.Projects._ref
+			});
+			return memo;
+		}, []);
+
+		var workspaceDeferred = $q.defer();
+
+		function queryNextProjectList() {
+			var projectRequest = queuedProjectRequests.shift();
+			projectsRefs[projectRequest.workspaceRef] = projectRequest.projectsRef
+
+			$log.info('Querying projects for workspace ' + projectRequest.workspaceRef);
+			var projectsUrl = getUrl(projectRequest.projectsRef, {pagesize:200});
+			$http.jsonp(projectsUrl).then(function(projectsResponse){
+				$log.info('projectsResponse', projectsResponse)
+
+				requests.push({
+					url: projectsUrl,
+					data: projectsResponse.data
+				});
+
+	// TODO 
 	// Iterations
 	// Test Sets
 	// Test Cases
 	// ...
 
 
+	// Project promise recursion:
+	//		don't drill into another Project until we are done with this one.
+	//		end the outer deferred until we're done iterating.
 
+				if (queuedProjectRequests.length > 0) { // TODO ... and previous iterations have not drilled all the way down to represent all data
+					queryNextProjectList(); // recurse
+				}
+				else {
+					workspaceDeferred.resolve();
+				}
+			});
+		}
 
-// end of the line
-		return $q.when();
+		assert(queuedProjectRequests.length > 0, 'we should have some projects or this will never resolve.')
+		queryNextProjectList();
+
+		return workspaceDeferred.promise;
+
+// ... I just want to push the final processing into a next promise, even if could happen here...
 	}).then(function() {
 		function getRefProperties() {
 			return ""
-				+ "        subscriptionRef: '" + subscriptionRef + "',\n"
-				+ "        workspacesRef: '" + workspacesRef + "',\n"
+				+ "        subscriptionRef: '" + sanitize(subscriptionRef) + "',\n"
+				+ "        workspacesRef: '" + sanitize(workspacesRef) + "',\n"
+				+ "        projectsRefs: " + JSON.stringify(sanitize(projectsRefs)).replace('{','{\n            ').replace(',',',\n            ').replace('}', '\n        }') + ",\n"
 		}
 		function getBackendCalls() {
 			var code = "";
 			for(var i = 0; i < requests.length; i++) {
 				code += "            $httpBackend\n"
-				code += "                .whenJSONP('" + requests[i].url + "')\n"
-				code += "                .respond(" + JSON.stringify(requests[i].data) + ");\n"
+				code += "                .whenJSONP('" + sanitize(requests[i].url) + "')\n"
+				code += "                .respond(" + JSON.stringify(sanitize(requests[i].data)) + ");\n"
 			}
 			return code;
 		}
-		$scope.code = "window.fakeBackend = (function(){\n"
+		$scope.code = "// This code is generated by GenerateMockBackendCtrl.js\n"
+			+ "window.fakeBackend = (function(){\n"
 			+ "    return {\n"
 			+ getRefProperties()
 			+ "        setup: function() {\n"
--- test/mocks/mock-rally-backend.js
+++ test/mocks/mock-rally-backend.js
@@ -1,53 +1,31 @@
-"use strict";
-
-// A reusable $httpBackend to stand in for Rally.
-
-// NOTE
-//		This is an experiment!
-//		I'm debating whether it's worth it to build a reusable backend for testing,
-//		as compared to mocking each response one-off as needed.
-//		The downside of one-off is that the responses are kind of complex, and used in many spots.
-
-// CONCERN
-//		When unit testing C# code, the mock and the actual code shared an interface.
-//		When you change the real code, any ad hoc or persistent mocks would often fail to compile due to interface changes, hinting that you must update the mocks.
-//		Without this here, if the real code changes, all of the code that uses it continues to pass if they are coded against an not-updated mock that no longer matches the concrete.
-//		I am concerned that as I go through this exercise I continually get to a point where all tests pass but the app is in a shambles.
-//		I suspect I am doing something wrong. :(
-
-// TODO
-//		Walk the Rally service and generate this implementation.
-//		Future versions can regenerate it and diff to see how the rally service has changed.
 
 window.fakeBackend = (function(){
+	"use strict";
 
 	return {
 		// TODO generation hard codes this
-		subscriptionRef: 'https://rally1.rallydev.com/slm/webservice/v3.0/subscription',
+
 		setup: function($httpBackend) {
 
-			$httpBackend
-				// TODO generation hard codes this
-				.whenJSONP('https://rally1.rallydev.com/slm/webservice/v3.0/subscription?jsonp=JSON_CALLBACK')
+			// Subscription
 
-				// TODO generation queries for this and sanitizes:
-				//		GUID and integer id's --> build a dictionary. replace with a new value and add to dictionary. replace same guid in future requests with the one created for this one.
-				//		text fields --> similar replacement scheme: Name="Name 1", Description="Description 1", etc
-				//		dates --> leave them?
-				//		known exceptions: _rallyAPIMajor, _objectVersion, etc --> leave unchanged
+			$httpBackend
+				.whenJSONP('https://rally1.rallydev.com/slm/webservice/v3.0/subscription?jsonp=JSON_CALLBACK')
 				.respond({"Subscription": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/subscription/15647602362", "_objectVersion": "2", "_refObjectName": "Community (HS_1) Edition - Innosphere SDG - parchedsquid@gmail.com", "CreationDate": "2013-12-05T15:15:19.513Z", "_CreatedAt": "8 minutes ago", "ObjectID": 15647602362, "ExpirationDate": null, "MaximumCustomUserFields": 5, "MaximumProjects": 1, "Modules": "", "Name": "Community (HS_1) Edition - Innosphere SDG - parchedsquid@gmail.com", "PasswordExpirationDays": 0, "PreviousPasswordCount": 0, "ProjectHierarchyEnabled": false, "RevisionHistory": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/revisionhistory/15647602363", "_type": "RevisionHistory"}, "SessionTimeoutSeconds": null, "StoryHierarchyEnabled": true, "StoryHierarchyType": "Limited", "SubscriptionID": 49713, "SubscriptionType": "Community (HS_1)", "Workspaces": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/Subscription/15647602362/Workspaces", "_type": "Workspace", "Count": 1}, "Errors": [], "Warnings": []}});
 
-			// Workspaces list
+			// Workspaces
 
 			$httpBackend
 				.whenJSONP('https://rally1.rallydev.com/slm/webservice/v2.0/Subscription/15647602362/Workspaces?jsonp=JSON_CALLBACK&pagesize=200')
 				.respond({"QueryResult": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "Errors": [], "Warnings": [], "TotalResultCount": 1, "StartIndex": 1, "PageSize": 200, "Results": [{"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/workspace/15647602518", "_objectVersion": "1", "_refObjectName": "Workspace 1", "CreationDate": "2013-12-05T15:15:19.525Z", "_CreatedAt": "today at 8:15 am", "ObjectID": 15647602518, "Subscription": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/subscription/15647602362", "_refObjectName": "Community (HS_1) Edition - Innosphere SDG - parchedsquid@gmail.com", "_type": "Subscription"}, "Description": "", "Name": "Workspace 1", "Owner": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/user/15647602512", "_refObjectName": "parchedsquid", "_type": "User"}, "Projects": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0","_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/Workspace/15647602518/Projects", "_type": "Project", "Count": 1},"RevisionHistory": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/revisionhistory/15647602519", "_type": "RevisionHistory"}, "SchemaVersion": "ca57f1d3a6e34fe5e581df2d5e397d57", "State": "Open", "Style": "UserStory", "TypeDefinitions": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/Workspace/15647602518/TypeDefinitions", "_type": "TypeDefinition", "Count": 50}, "WorkspaceConfiguration": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/workspaceconfiguration/15647602520", "_type": "WorkspaceConfiguration"}, "_type": "Workspace"}]}});
 
-			// Projects for Workspace 1
+			// Projects
 
 			$httpBackend
 				.whenJSONP('https://rally1.rallydev.com/slm/webservice/v2.0/Workspace/15647602518/Projects?jsonp=JSON_CALLBACK&pagesize=200')
 				.respond({"QueryResult": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "Errors": [], "Warnings": [], "TotalResultCount": 1, "StartIndex": 1, "PageSize": 200, "Results": [{"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/project/15647602608", "_objectVersion": "1", "_refObjectName": "Sample Project", "CreationDate": "2013-12-05T15:15:19.528Z", "_CreatedAt": "today at 8:15 am", "ObjectID": 15647602608, "Subscription": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/subscription/15647602362", "_refObjectName": "Community (HS_1) Edition - Innosphere SDG - parchedsquid@gmail.com", "_type": "Subscription"}, "Workspace": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/workspace/15647602518", "_refObjectName": "Workspace 1", "_type": "Workspace"}, "BuildDefinitions": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/Project/15647602608/BuildDefinitions", "_type": "BuildDefinition", "Count": 1}, "Children": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/Project/15647602608/Children", "_type": "Project", "Count": 0}, "Description": "", "Editors": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/Project/15647602608/Editors", "_type": "User", "Count": 1}, "Iterations": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/Project/15647602608/Iterations", "_type": "Iteration", "Count": 1}, "Name": "Sample Project", "Owner": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/user/15647602512", "_refObjectName": "parchedsquid", "_type": "User"}, "Parent": null, "SchemaVersion": "ca57f1d3a6e34fe5e581df2d5e397d57", "State": "Open", "TeamMembers": {"_rallyAPIMajor": "2", "_rallyAPIMinor": "0", "_ref": "https://rally1.rallydev.com/slm/webservice/v2.0/Project/15647602608/TeamMembers", "_type": "User", "Count": 0}, "_type": "Project"}]}})
+
+			// Iterations
 
 			$httpBackend
 				.whenJSONP('https://rally1.rallydev.com/slm/webservice/v2.0/Project/15647602608/Iterations?jsonp=JSON_CALLBACK&pagesize=200')
