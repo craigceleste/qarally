250e41ea518015b40081c679155202990305e4e6
Craig Celeste <craigc@innosphere.ca>
2014-01-23 21:26:37 -0500

    experimenting with tests

--- test/unit/services/RallySpec.js
+++ test/unit/services/RallySpec.js
@@ -4,6 +4,7 @@ describe('Rally', function() {
 
 	var rally;
 	var mockWindow, $rootScope, $httpBackend;
+	var fakeBackend = window.fakeBackend; // TODO find a better way to inject it
 
 	beforeEach(function(){
 
@@ -20,7 +21,7 @@ describe('Rally', function() {
 		inject(function(_$rootScope_, $injector){
 			$rootScope = _$rootScope_;
 			$httpBackend = $injector.get('$httpBackend');
-			window.fakeBackend.setup($httpBackend);
+			fakeBackend.setup($httpBackend);
 		});
 
 		inject(function(Rally){
@@ -35,87 +36,107 @@ describe('Rally', function() {
 
 	it('getSubscriptionData prepares request correctly and extracts data correctly from the response.', function() {
 
-		rally.getSubscriptionData().then(function(subscriptionData) {
+		var thenCalled = false;
+		
+		rally
+			.getSubscriptionData()
+			.then(function(subscriptionData) {
 
-			expect(subscriptionData._ref).toEqual('https://rally1.rallydev.com/slm/webservice/v2.0/subscription/15647602362');
-			expect(subscriptionData.workspacesRef).toEqual('https://rally1.rallydev.com/slm/webservice/v2.0/Subscription/15647602362/Workspaces');
+				thenCalled = true;
+				expect(subscriptionData._ref).toEqual(fakeBackend.subscription.data.Subscription._ref);
+				expect(subscriptionData.workspacesRef).toEqual(fakeBackend.subscription.data.Subscription.Workspaces._ref);
 
-		});
+			});
 
 		$httpBackend.flush();
+		expect(thenCalled).toBe(true);
 	});
 
 	it('getWorkspaceList prepares request correctly and extracts data correctly from the response.', function() {
 
-		rally.getWorkspaceList('https://rally1.rallydev.com/slm/webservice/v2.0/Subscription/15647602362/Workspaces').then(function(workspaceList) {
+		var thenCalled = false;
 
-			expect(workspaceList).toEqual([{
-				_ref: 'https://rally1.rallydev.com/slm/webservice/v2.0/workspace/15647602518',
-				name: 'Workspace 1',
-				projectsRef: 'https://rally1.rallydev.com/slm/webservice/v2.0/Workspace/15647602518/Projects'
-			}]);
+		rally
+			.getWorkspaceList(fakeBackend.workspaceList.inputs.workspacesRef)
+			.then(function(workspaceList) {
 
-		});
+				thenCalled = true;
+				expect(workspaceList.length).toEqual(fakeBackend.workspaceList.data.QueryResult.Results.length)
+				expect(workspaceList[0]._ref).toEqual(fakeBackend.workspaceList.data.QueryResult.Results[0]._ref);
+				expect(workspaceList[0].name).toEqual(fakeBackend.workspaceList.data.QueryResult.Results[0].Name);
+				expect(workspaceList[0].projectsRef).toEqual(fakeBackend.workspaceList.data.QueryResult.Results[0].Projects._ref);
+			});
 
 		$httpBackend.flush();
+		expect(thenCalled).toBe(true);
 	});
 
 	it('getProjectList prepares request correctly and extracts data correctly from the response.', function() {
 
-		rally.getProjectList('https://rally1.rallydev.com/slm/webservice/v2.0/Workspace/15647602518/Projects').then(function(projectList) {
+		var thenCalled = false;
+		rally
+			.getProjectList(fakeBackend.projectList.inputs.projectsRef)
+			.then(function(projectList) {
 
-			expect(projectList).toEqual([{
-				_ref: 'https://rally1.rallydev.com/slm/webservice/v2.0/project/15647602608',
-				name: 'Sample Project',
-				iterationsRef: 'https://rally1.rallydev.com/slm/webservice/v2.0/Project/15647602608/Iterations'
-				}]);
-
-		});
+				thenCalled = true;
+				expect(projectList.length).toEqual(fakeBackend.projectList.data.QueryResult.Results.length);
+				expect(projectList[0]._ref).toEqual(fakeBackend.projectList.data.QueryResult.Results[0]._ref);
+				expect(projectList[0].name).toEqual(fakeBackend.projectList.data.QueryResult.Results[0].Name);
+				expect(projectList[0].iterationsRef).toEqual(fakeBackend.projectList.data.QueryResult.Results[0].Iterations._ref);
+			});
 
 		$httpBackend.flush();
+		expect(thenCalled).toBe(true);
 	});
 
 	it('getIterationList prepares request correctly and extracts data correctly from the response.', function() {
 
-		rally.getIterationList('https://rally1.rallydev.com/slm/webservice/v2.0/Project/15647602608/Iterations').then(function(iterationList) {
+		var thenCalled = false;
+		rally
+			.getIterationList(fakeBackend.iterationList.inputs.iterationsRef)
+			.then(function(iterationList) {
 
-			expect(iterationList).toEqual([{
-				_ref: 'https://rally1.rallydev.com/slm/webservice/v2.0/iteration/15647935070',
-				name: 'Sprint 1',
-				startDate: '2013-12-16T07:00:00.000Z',
-				endDate: '2013-12-23T06:59:59.000Z'
-				}]);
-
-		});
+				thenCalled = true;
+				expect(iterationList.length).toEqual(fakeBackend.iterationList.data.QueryResult.Results.length);
+				expect(iterationList[0]._ref).toEqual(fakeBackend.iterationList.data.QueryResult.Results[0]._ref);
+				expect(iterationList[0].name).toEqual(fakeBackend.iterationList.data.QueryResult.Results[0].Name);
+				expect(iterationList[0].startDate).toEqual(fakeBackend.iterationList.data.QueryResult.Results[0].StartDate);
+				expect(iterationList[0].endDate).toEqual(fakeBackend.iterationList.data.QueryResult.Results[0].EndDate);
+			});
 
 		$httpBackend.flush();
+		expect(thenCalled).toBe(true);
 	});
 
 	it('getAllSubscriptionData traverses the promises correctly and aggregates data correctly', function() {
 
-		rally.getAllSubscriptionData().then(function(subscriptionData) {
+		var thenCalled = false;
+		rally
+			.getAllSubscriptionData()
+			.then(function(subscriptionData) {
 
-			expect(subscriptionData._ref).toEqual('https://rally1.rallydev.com/slm/webservice/v2.0/subscription/15647602362');
-			expect(subscriptionData.workspacesRef).toEqual('https://rally1.rallydev.com/slm/webservice/v2.0/Subscription/15647602362/Workspaces');
+				thenCalled = true;
+				expect(subscriptionData._ref).toEqual(fakeBackend.subscription.data.Subscription._ref);
+				expect(subscriptionData.workspacesRef).toEqual(fakeBackend.subscription.data.Subscription.Workspaces._ref);
+				var workspaceRef = 'https://rally1.rallydev.com/slm/webservice/v2.0/workspace/15647602518';
+				expect(subscriptionData.workspaces[workspaceRef]._ref).toEqual(workspaceRef);
+				expect(subscriptionData.workspaces[workspaceRef].name).toEqual('Workspace 1');
+				expect(subscriptionData.workspaces[workspaceRef].projectsRef).toEqual('https://rally1.rallydev.com/slm/webservice/v2.0/Workspace/15647602518/Projects');
 
-			var workspaceRef = 'https://rally1.rallydev.com/slm/webservice/v2.0/workspace/15647602518';
-			expect(subscriptionData.workspaces[workspaceRef]._ref).toEqual(workspaceRef);
-			expect(subscriptionData.workspaces[workspaceRef].name).toEqual('Workspace 1');
-			expect(subscriptionData.workspaces[workspaceRef].projectsRef).toEqual('https://rally1.rallydev.com/slm/webservice/v2.0/Workspace/15647602518/Projects');
+				var projectRef = 'https://rally1.rallydev.com/slm/webservice/v2.0/project/15647602608';
+				expect(subscriptionData.workspaces[workspaceRef].projects[projectRef]._ref).toEqual(projectRef);
+				expect(subscriptionData.workspaces[workspaceRef].projects[projectRef].name).toEqual('Sample Project');
+				expect(subscriptionData.workspaces[workspaceRef].projects[projectRef].iterationsRef).toEqual('https://rally1.rallydev.com/slm/webservice/v2.0/Project/15647602608/Iterations');
 
-			var projectRef = 'https://rally1.rallydev.com/slm/webservice/v2.0/project/15647602608';
-			expect(subscriptionData.workspaces[workspaceRef].projects[projectRef]._ref).toEqual(projectRef);
-			expect(subscriptionData.workspaces[workspaceRef].projects[projectRef].name).toEqual('Sample Project');
-			expect(subscriptionData.workspaces[workspaceRef].projects[projectRef].iterationsRef).toEqual('https://rally1.rallydev.com/slm/webservice/v2.0/Project/15647602608/Iterations');
-
-			var iterationRef = 'https://rally1.rallydev.com/slm/webservice/v2.0/iteration/15647935070';
-			expect(subscriptionData.workspaces[workspaceRef].projects[projectRef].iterations[iterationRef]._ref).toEqual(iterationRef);
-			expect(subscriptionData.workspaces[workspaceRef].projects[projectRef].iterations[iterationRef].name).toEqual('Sprint 1');
-			expect(subscriptionData.workspaces[workspaceRef].projects[projectRef].iterations[iterationRef].startDate).toEqual('2013-12-16T07:00:00.000Z');
-			expect(subscriptionData.workspaces[workspaceRef].projects[projectRef].iterations[iterationRef].endDate).toEqual('2013-12-23T06:59:59.000Z');
-		});
+				var iterationRef = 'https://rally1.rallydev.com/slm/webservice/v2.0/iteration/15647935070';
+				expect(subscriptionData.workspaces[workspaceRef].projects[projectRef].iterations[iterationRef]._ref).toEqual(iterationRef);
+				expect(subscriptionData.workspaces[workspaceRef].projects[projectRef].iterations[iterationRef].name).toEqual('Sprint 1');
+				expect(subscriptionData.workspaces[workspaceRef].projects[projectRef].iterations[iterationRef].startDate).toEqual('2013-12-16T07:00:00.000Z');
+				expect(subscriptionData.workspaces[workspaceRef].projects[projectRef].iterations[iterationRef].endDate).toEqual('2013-12-23T06:59:59.000Z');
+			});
 
 		$httpBackend.flush();
+		expect(thenCalled).toBe(true);
 	});
 
 	it('initSubscriptionData will return a cached version.', function() {
