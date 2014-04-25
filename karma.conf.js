f0a621db021bdd06fcc649ba429da692097a3d1f
Craig Celeste <craigc@innosphere.ca>
2014-04-25 19:23:07 -0400

    Adding karma.conf.js

---
+++ karma.conf.js
@@ -0,0 +1,67 @@
+// Karma configuration
+// http://karma-runner.github.io/0.10/config/configuration-file.html
+
+module.exports = function(config) {
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+    // testing framework to use (jasmine/mocha/qunit/...)
+    frameworks: ['jasmine'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      // third party libraries
+      'app/bower_components/underscore/underscore.js',
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-sanitize/angular-sanitize.js',
+      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/jquery/dist/jquery.js', // TODO remove: angular code should not use jquery.
+
+      // testing tools
+      'app/bower_components/angular-mocks/angular-mocks.js',
+      'test/mocks/**/*.js',
+
+      // code under test
+      'app/scripts/*.js',
+      'app/scripts/**/*.js',
+      'app/pages/**/*.js',
+
+      // tests
+      'test/**/*Spec.js'
+    ],
+
+    // list of files / patterns to exclude
+    exclude: [
+      // contains old-school DOM manipulation. not tested
+      'app/scripts/site.js'
+    ],
+
+    // web server port
+    port: 9876,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
