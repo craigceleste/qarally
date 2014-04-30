c550775c693a57d3fbd2196ab327e31835ed6a15
Craig Celeste <craigc@innosphere.ca>
2014-04-30 18:07:17 -0400

    do not hash image names in dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -160,7 +160,7 @@ module.exports = function (grunt) {
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/pages/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
-            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+            // TODO my views are not referencing the hashed file name. figure out why.  '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
             '<%= yeoman.dist %>/styles/fonts/*'
           ]
         }
